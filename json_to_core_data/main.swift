598a79f22c92bfec836fefd49a0b3949ab6130fc
Developers <developer1@weinand.it>
2019-01-03 11:06:28 +0200

    Initial Commit

---
+++ json_to_core_data/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  json_to_core_data
+//
+//  Created by Shvetsov Vitalii on 03/01/2019.
+//  Copyright © 2019 Shvetsov Vitalii. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
