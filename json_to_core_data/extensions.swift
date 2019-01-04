612c530a2caf0f23ce248940e134dd37ba31851b
Developers <developer1@weinand.it>
2019-01-04 16:38:09 +0200

    Added removing restricted variable names, boolean variables, inner entities

--- json_to_core_data/extensions.swift
+++ json_to_core_data/extensions.swift
@@ -11,3 +11,55 @@ extension NSDate {
         return df.date(from: string) as NSDate?
     }
 }
+
+
+func isStorageType(value: Any) -> Bool {
+    return value is [String: Any]
+        || value is [[String: Any]]
+        || value is [String]
+}
+
+
+func cocoaTypeFor(value: (String, Any)) -> String {
+    return dataTypeFor(value: value, string: "string", date: "date",
+                       int64: "integer", float: "float", bool: "boolean")
+}
+
+
+func coreDataTypeFor(value: (String, Any)) -> String {
+    return dataTypeFor(value: value, string: "String", date: "Date",
+                       int64: "Integer 64", float: "Float", bool: "Boolean")
+}
+
+
+private func dataTypeFor(value: (String, Any),
+                         string: String,
+                         date: String,
+                         int64: String,
+                         float: String,
+                         bool: String) -> String {
+    if value.0.starts(with: "is") { return bool }
+    
+    var type = string
+    if value.1 is String {
+        if NSDate.from(string: value.1 as! String) != nil {
+            type = date
+        } else if isBoolean(value) { type = bool }
+    } else if value.1 is Int64 || value.1 is Int {
+        type = int64
+    } else if value.1 is Float || value.1 is Double {
+        type = float
+    } else if value.1 is Bool {
+        type = bool
+    }
+    
+    return type
+}
+
+
+private func isBoolean(_ value: Any) -> Bool {
+    return value is Bool
+        ||  (value is String
+            && ((value as! String).caseInsensitiveCompare("true") == .orderedSame
+                || (value as! String).caseInsensitiveCompare("false") == .orderedSame))
+}
--- json_to_core_data/main.swift
+++ json_to_core_data/main.swift
@@ -9,41 +9,50 @@ private func spawnJSONIncteractionWith(json: [String: Any]) {
         fileUpdater.seekToEndOfFile()
         
         for obj in json {
-            var className = String(obj.key[..<String.Index(encodedOffset: obj.key.count - 1)])
-            className = className.prefix(1).uppercased() + className.dropFirst()
-            
-            if let dictArray = obj.value as? [[String: Any]], obj.key != "map" {
-                let dict = dictArray[0]
-                
-                createClassFor(file: fileUpdater, name: className, with: dict)
-                createCoreDataModelFor(file: fileUpdater, className: className, with: dict)
-                fileUpdater.write("\n\n\n".data(using: .utf8)!)
-            } else if let arrayValues = obj.value as? [String] {
-                let value = arrayValues[0]
-                
-                let lowerCasedClassName = className.prefix(1).lowercased() + className.dropFirst()
-                createClassFor(file: fileUpdater, name: className, with: [lowerCasedClassName: value])
-                createCoreDataModelFor(file: fileUpdater, className: className, with: [lowerCasedClassName: value])
-                fileUpdater.write("\n\n\n".data(using: .utf8)!)
-            }
+            spawnEntityFor(file: fileUpdater, key: obj.key, with: obj.value)
         }
     }
 }
 
+private func spawnEntityFor(file: FileHandle, key: String, with object: Any) {
+    var className = String(key[..<String.Index(encodedOffset: key.count - 1)])
+    className = className.prefix(1).uppercased() + className.dropFirst()
+    
+    if let dictArray = object as? [[String: Any]] {
+        let dict = dictArray[0]
 
-private func createClassFor(file: FileHandle, name: String, with object: [String: Any]) {
-    file.write("   @objc(\(name))\n".data(using: .utf8)!)
+        spawnEntityFor(file: file, className: className, with: dict)
+    } else if let arrayValues = object as? [String] {
+        let value = arrayValues[0]
+        let lowerCasedClassName = className.prefix(1).lowercased() + className.dropFirst()
+        
+        createClassFor(file: file, name: className, with: [lowerCasedClassName: value])
+        createCoreDataModelFor(file: file, className: className, with: [lowerCasedClassName: value])
+    } else if let dict = object as? [String: Any] {
+        spawnEntityFor(file: file, className: key.prefix(1).uppercased() + key.dropFirst(), with: dict)
+    }
+}
+
+
+private func spawnEntityFor(file: FileHandle, className: String, with object: [String: Any]) {
+    file.write("\n\n\n".data(using: .utf8)!)
+    createClassFor(file: file, name: className, with: object)
+    createCoreDataModelFor(file: file, className: className, with: object)
+}
+
+
+private func createClassFor(file: FileHandle, name: String, with values: [String: Any]) {
     file.write("   public class \(name): NSManagedObject {\n".data(using: .utf8)!)
-    createConstantsFor(file: file, with: object, className: name)
+    createConstantsFor(file: file, with: values, className: name)
     file.write("\n\n".data(using: .utf8)!)
-    createSaveDataMethodFor(file: file, with: object, className: name)
+    createSaveDataMethodFor(file: file, with: values, className: name)
     file.write("\n\n".data(using: .utf8)!)
-    createToDictionaryMethodFor(file: file, with: object, className: name)
+    createToDictionaryMethodFor(file: file, with: values, className: name)
     file.write("\n   }\n".data(using: .utf8)!)
 }
 
 
-private func createCoreDataModelFor(file: FileHandle, className: String, with object: [String: Any]) {
+private func createCoreDataModelFor(file: FileHandle, className: String, with values: [String: Any]) {
     let entityElement = XMLElement(name: "entity")
     entityElement.addAttribute(XMLNode.attribute(withName: "name",
                                                  stringValue: className) as! XMLNode)
@@ -52,47 +61,53 @@ private func createCoreDataModelFor(file: FileHandle, className: String, with ob
     
     let entity = XMLDocument(rootElement: entityElement)
     
-    for innerObj in object {
-        var attributes = [XMLNode]()
-        attributes.append(XMLNode.attribute(withName: "name",
-                                             stringValue: innerObj.key) as! XMLNode)
-        attributes.append(XMLNode.attribute(withName: "optional",
-                                            stringValue: "YES") as! XMLNode)
-        attributes.append(XMLNode.attribute(withName: "attributeType",
-                                            stringValue: entityTypeFor(value: innerObj)) as! XMLNode)
-        attributes.append(XMLNode.attribute(withName: "defaultValueString",
-                                            stringValue: "0") as! XMLNode)
-        attributes.append(XMLNode.attribute(withName: "usesScalarValueType",
-                                            stringValue: entityTypeFor(value: innerObj) != "Date" ? "YES" : "NO") as! XMLNode)
-        attributes.append(XMLNode.attribute(withName: "syncable",
-                                            stringValue: "YES") as! XMLNode)
+    for innerObj in values {
+        let modifiedKey = forbiddenVariableNames[innerObj.key] ?? innerObj.key
+        let type = coreDataTypeFor(value: innerObj)
+        let attributes = [
+            XMLNode.attribute(withName: "name", stringValue: modifiedKey) as! XMLNode,
+            XMLNode.attribute(withName: "optional", stringValue: "YES") as! XMLNode,
+            XMLNode.attribute(withName: "attributeType", stringValue: type) as! XMLNode,
+            XMLNode.attribute(withName: "defaultValueString", stringValue: "0") as! XMLNode,
+            XMLNode.attribute(withName: "usesScalarValueType",
+                              stringValue: type == "Date" ? "NO" : "YES") as! XMLNode,
+            XMLNode.attribute(withName: "syncable", stringValue: "YES") as! XMLNode
+        ]
         
         let node = XMLNode.element(withName: "attribute", children: nil, attributes: attributes) as! XMLNode
         entityElement.addChild(node)
     }
     
     file.write(entity.xmlData(options: .nodePrettyPrint))
-}
-
-
-private func createConstantsFor(file: FileHandle, with object: [String: Any], className: String) {
-    let staticLet = "\tstatic let"
-    file.write("\(staticLet) SQL_TABLE = \"\(className)\"".data(using: .utf8)!)
     
-    for key in object.keys {
-        file.write("\n\(staticLet) \(key)Sync = \"\(key)\"".data(using: .utf8)!)
+    for innerObj in values {
+        if isStorageType(value: innerObj.1) {
+            spawnEntityFor(file: file, key: innerObj.0, with: innerObj.1)
+        }
     }
 }
 
 
-private func createToDictionaryMethodFor(file: FileHandle, with object: [String: Any], className: String) {
+private func createConstantsFor(file: FileHandle, with values: [String: Any], className: String) {
+    let staticLet = "\tstatic let"
+    file.write("\(staticLet) SQL_TABLE = \"\(className)\"".data(using: .utf8)!)
+    
+    for key in values.keys {
+        let modifiedKey = forbiddenVariableNames[key] ?? key
+        file.write("\n\(staticLet) \(key)Sync = \"\(modifiedKey)\"".data(using: .utf8)!)
+    }
+}
+
+
+private func createToDictionaryMethodFor(file: FileHandle, with values: [String: Any], className: String) {
     let funcToDictionary = "\tfunc toDictionary() -> [String: Any] {\n"
     
     file.write(funcToDictionary.data(using: .utf8)!)
     file.write("\t  var jsonDict = [String: Any]()".data(using: .utf8)!)
     
-    for key in object.keys {
-        file.write("\n\t  jsonDict[\(className).\(key)Sync] = \(key)".data(using: .utf8)!)
+    for key in values.keys {
+        let modifiedKey = forbiddenVariableNames[key] ?? key
+        file.write("\n\t  jsonDict[\(className).\(key)Sync] = \(modifiedKey)".data(using: .utf8)!)
     }
     
     file.write("\n\t  return jsonDict".data(using: .utf8)!)
@@ -100,16 +115,18 @@ private func createToDictionaryMethodFor(file: FileHandle, with object: [String:
 }
 
 
-private func createSaveDataMethodFor(file: FileHandle, with object: [String: Any], className: String) {
+private func createSaveDataMethodFor(file: FileHandle, with values: [String: Any], className: String) {
     let funcSaveData = "\tstatic func saveData(data: [String: Any], " +
-            "with context: NSManagedObjectContext, \n\tisFromSync: Bool) -> \(className) {\n"
+            "with context: NSManagedObjectContext, \n\t   isFromSync: Bool) -> \(className) {\n"
     
     file.write(funcSaveData.data(using: .utf8)!)
     file.write("\t  let object = \(className)(context: context)".data(using: .utf8)!)
     
-    for innerObj in object {
-        file.write(("\n\t  object.\(innerObj.key) = " +
-            "AppCredentials.\(typeFor(value: innerObj))FromObject(object: data[\(innerObj.0)Sync])").data(using: .utf8)!)
+    for innerObj in values {
+        let type = cocoaTypeFor(value: innerObj)
+        let modifiedKey = forbiddenVariableNames[innerObj.key] ?? innerObj.key
+        file.write(("\n\t  object.\(modifiedKey) = " +
+            "AppCredentials.\(type)FromObject(object: data[\(innerObj.0)Sync])").data(using: .utf8)!)
     }
     
     file.write("\n\t  return object".data(using: .utf8)!)
@@ -119,8 +136,8 @@ private func createSaveDataMethodFor(file: FileHandle, with object: [String: Any
 
 private func readJson() -> [String: Any]? {
     let path = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("jsonInput.txt")
-    if let json = try? JSONSerialization.jsonObject(
-        with: Data(contentsOf: path), options: JSONSerialization.ReadingOptions.mutableContainers),
+    if let json = try? JSONSerialization.jsonObject(with: Data(contentsOf: path),
+                                                    options: .mutableContainers),
         let jsonDict = json as? [String: Any] {
         return jsonDict
     }
@@ -129,46 +146,7 @@ private func readJson() -> [String: Any]? {
 }
 
 
-private func typeFor(value: (String, Any)) -> String {
-    var type = "string"
-    if value.1 is String {
-        if NSDate.from(string: value.1 as! String) != nil {
-            type = "date"
-        } else {
-            type = "string"
-        }
-    } else if value.1 is Int64 || value.1 is Int {
-        type = "integer"
-    } else if value.1 is Float || value.1 is Double {
-        type = "float"
-    } else if value.1 is Bool {
-        type = "boolean"
-    } else {
-        type = "none"
-    }
-    
-    return type
-}
-
-
-private func entityTypeFor(value: (String, Any)) -> String {
-    var type = "String"
-    if value.1 is String {
-        if NSDate.from(string: value.1 as! String) != nil {
-            type = "Date"
-        } else {
-            type = "String"
-        }
-    } else if value.1 is Int64 || value.1 is Int {
-        type = "Integer 64"
-    } else if value.1 is Float || value.1 is Double {
-        type = "Float"
-    } else if value.1 is Bool {
-        type = "Boolean"
-    }
-    
-    return type
-}
+let forbiddenVariableNames = ["isDeleted" : "isDeletedSync"]
 
 
 fileprivate func main() {
